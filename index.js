0c796675a658d122e019bd8dccdc849ddc8b92bb
vermad843 <vishnuverma7524@gmail.com>
2022-07-23 10:04:42 +0530

    ... operator

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-let obj1 = { 
-    name:"greetings"
-   }
+//  obj is a reference type 
 
-let obj2 = obj1
-   obj1.age=50
+// let obj1 = { 
+//     name:"greetings"
+//    }
 
-   console.log(obj1, obj2);//{name: 'greetings', age: 50} {name: 'greetings', age: 50} 
+// let obj2 = obj1
+//    obj1.age=50
+
+//    console.log(obj1, obj2);//{name: 'greetings', age: 50} {name: 'greetings', age: 50} 
+
+
+
+//   {[{}]} - spread operator
+
+
+   const a  =  {
+        test: [{
+        test: [{test: 287367823}]
+        }]
+    }
+
+    const b = {...a}
+    console.log(b);
+
+    
